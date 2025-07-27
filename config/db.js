2e946bdf3d295ddf337ef6f3914f17f550abf53d
hdev916 <harsh@kascommerce.com>
2025-07-27 23:01:10 +0530

    done

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 module.exports = () => {
-  mongoose.connect('mongodb+srv://harsh:Rambo125636@cluster0.ljpmvgm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => console.log('MongoDB Connected'))
+  mongoose.connect('mongodb+srv://Harsh:Rambo125636@cluster0.ljpmvgm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => console.log('MongoDB Connected'))
     .catch((err) => console.error('MongoDB connection error:', err));
 };
